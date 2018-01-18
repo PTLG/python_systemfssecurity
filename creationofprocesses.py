47816cff816d51769620b89529d8354d1b03cbe9
Zubel Tomasz <kontowykopkowe@gmail.com>
2018-01-18 13:11:57 +0100

    psutil module - analysiss of system processes, this should be tested on the non-domain environment(because of access denied errors when users attempts to open any kind of program, there is no problems with actually existing processes like explorer.exe or chrome.exe, for example))

--- creationofprocesses.py
+++ creationofprocesses.py
@@ -1,13 +1,25 @@
-# import psutil
-# import wmi, pythoncom, os
-#
-# try:
-#     pythoncom.CoInitialize()
-#     connection = wmi.WMI()
-#     watcher = connection.Win32_Process.watch_for("creation")
-#
-#     while True:
-#         new_process = watcher()
-#             if:
-#                 len(psutil.Process(new_process.ProcessID).cmdline()) == 2 and (os.path.exists(psutil.Process(new_process.ProcessID).cmdline()):
-#                     print("PID: ", new_process.ProcessID)
+import psutil
+import wmi, pythoncom, os
+
+try:
+    pythoncom.CoInitialize()
+    connection = wmi.WMI()
+    watcher = connection.Win32_Process.watch_for("creation")
+
+    while True:
+        new_process = watcher()
+        if len(psutil.Process(new_process.ProcessID).cmdline()) == 2 and (os.path.exists(psutil.Process(new_process.ProcessID).cmdline()[1])):
+            print("PID: ", new_process.ProcessID)
+            print("Name: ", new_process.Caption)
+            print("Priority: ", new_process.Priority)
+            print("Filename: ", psutil.Process(new_process.ProcessID).cmdline()[1])
+            print("Software used: ", psutil.Process(new_process.ProcessID).cmdline()[0])
+        if psutil.Process(new_process.ParentProcessId):
+            print("Parent PID: ", new_process.ParentProcessId)
+            print("Parent Name: ", psutil.Process(new_process.ParentProcessId).name())
+            print("Parent Executable: ", psutil.Process(new_process.ParentProcessId).cmdline()[0])
+
+except KeyboardInterrupt as e:
+    print(e)
+finally:
+    pythoncom.CoUninitialize()
